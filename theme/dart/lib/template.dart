f642c9471fa0d47a88f61e6706bac94c3bfba5c2
Siong-Ui Te <siongui@gmail.com>
2013-12-05 02:16:12 +0800

    extract syntax in template

--- theme/dart/lib/template.dart
+++ theme/dart/lib/template.dart
@@ -10,14 +10,30 @@ class template {
   }
 
   Future<String> render(String tempalte, Map<String, Object> data) {
+    // render template in async way
     final File file = new File(
         path.join(this.templates_dir, tempalte));
-    return file.readAsString();
+    return file.readAsString().then((String content) {
+      return processTemplateSyntax(content);
+    });
   }
 
   String renderSync(String tempalte, Map<String, Object> data) {
+    // render template in sync way
     final File file = new File(
         path.join(this.templates_dir, tempalte));
-    return file.readAsStringSync();
+    return processTemplateSyntax(file.readAsStringSync());
+  }
+
+  String processTemplateSyntax(String template_content) {
+    // input: content in template file
+
+    var variables = new RegExp(r'{{([^}}]+)?}}');
+
+    for (Match match in variables.allMatches(template_content)) {
+      print(match.group(0));
+    }
+
+    return template_content;
   }
 }
