2017a382e6b014d1c74f784a582ba6dd4ca89e74
Siong-Ui Te <siongui@gmail.com>
2014-01-26 02:34:42 +0800

    use template add use compiled js if not dartium

--- tool/server.go
+++ tool/server.go
@@ -3,16 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 )
 
+const tmplFilePath = "view/index.html"
+var tmpl, _ = template.ParseFiles(tmplFilePath)
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("URL: ", r.URL.Path)
-	fmt.Fprint(w, "hello world")
+	tmpl.Execute(w, nil)
 }
 
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.ListenAndServe(":8080", nil)
 }
