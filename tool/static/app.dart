bb5b9f965ac81b7a554cc8fc943a31168b5024e8
Siong-Ui Te <siongui@gmail.com>
2014-01-26 05:00:03 +0800

    basic form and listen to onclick of button

--- tool/static/app.dart
+++ tool/static/app.dart
@@ -1,4 +1,11 @@
 import 'dart:html';
 
+
+void sendUrl(Event e) {
+  ButtonElement elm = e.target as ButtonElement;
+  elm.text = "123";
+}
+
 void main() {
+  query("#getBtn").onClick.listen(sendUrl);
 }
