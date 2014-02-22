b54ab9b1763514391d28b53c4f8728968441ae16
Siong-Ui Te <siongui@gmail.com>
2014-02-22 23:57:35 +0800

    add more tags and build data structure for writing rst

--- tool/reader/json2rst.py
+++ tool/reader/json2rst.py
@@ -5,8 +5,20 @@ import os
 import json
 
 if __name__ == '__main__':
+  langTagLink = {}
+
   for path in os.listdir('.'):
     if path.startswith("http"):
       with open(path, 'r') as f:
         j = json.loads(f.read())
-	print(j)
+	#print(j)
+
+	if j["Language"] not in langTagLink:
+          langTagLink[j["Language"]] = {}
+
+	if j['Tag'] not in langTagLink[j["Language"]]:
+          langTagLink[j["Language"]][j['Tag']] = []
+
+	langTagLink[j["Language"]][j['Tag']].append(j)
+
+  print(langTagLink)
