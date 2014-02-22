9bd8e8dba497765faed8d43164d30a59c93a48fc
Siong-Ui Te <siongui@gmail.com>
2014-02-23 03:58:07 +0800

    move links to a folder

--- tool/reader/mylib/savelink.go
+++ tool/reader/mylib/savelink.go
@@ -1,3 +1,6 @@
+// http://stackoverflow.com/questions/10510691/how-to-check-whether-a-file-or-directory-denoted-by-a-path-exists-in-golang
+// http://stackoverflow.com/questions/14249467/os-mkdir-and-os-mkdirall-permission-value
+// http://stackoverflow.com/questions/1760757/how-to-efficiently-concatenate-strings-in-go
 package mylib
 
 import (
@@ -5,6 +8,7 @@ import (
 	"encoding/json"
 	"os"
 	"io/ioutil"
+	"fmt"
 	"log"
 )
 
@@ -19,7 +23,16 @@ type LinkInfo struct {
 }
 
 func SaveLinkAsJson(l LinkInfo) {
-	path := url.QueryEscape(l.Link)
+	const linkdir = "./links/"
+	if _, err := os.Stat(linkdir); err != nil {
+		if os.IsNotExist(err) {
+			os.Mkdir(linkdir, 0755)
+		} else {
+			log.Println(err)
+		}
+	}
+
+	path := fmt.Sprint(linkdir, url.QueryEscape(l.Link))
 	os.Remove(path)
 
 	b, err := json.Marshal(l)
