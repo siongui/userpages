08b4cdb869373ab37d29fbd91fb1c42f19360888
Siong-Ui Te <siongui@gmail.com>
2015-01-26 01:15:44 +0800

    add pelican config

---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Siong-Ui Te'
+SITENAME = u'Theory and Practice'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'theme'
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['i18n_subsites']
+
+# mapping: language_code -> settings_overrides_dict
+I18N_SUBSITES = {
+  'zh': {
+    'SITENAME': '理論與實作',
+    'AUTHOR': '戴上為',
+  }
+}
+
+DIRECT_TEMPLATES = ['index']
+PAGE_ORDER_BY = 'order'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
