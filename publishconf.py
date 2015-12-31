7e4783311e2a69efd4541f881fcc940d7e11f345
Siong-Ui Te <siongui@gmail.com>
2016-01-01 03:01:55 +0800

    use https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://siongui.github.io'
+SITEURL = 'https://siongui.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
