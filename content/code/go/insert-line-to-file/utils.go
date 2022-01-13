6268f255cb0d687bfe8aabcd0d188fe33c363faf
Víctor Briseño <victor.briseno.leon@hotmail.com>
2022-01-13 11:12:24 -0600

    easy reading and performance

--- content/code/go/insert-line-to-file/utils.go
+++ content/code/go/insert-line-to-file/utils.go
@@ -2,51 +2,36 @@ package insert
 
 import (
 	"bufio"
-	"io"
 	"io/ioutil"
+	"log"
 	"os"
+	"strings"
 )
 
-func File2lines(filePath string) ([]string, error) {
-	f, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	return LinesFromReader(f)
-}
-
-func LinesFromReader(r io.Reader) ([]string, error) {
-	var lines []string
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	return lines, nil
-}
-
-/**
- * Insert sting to n-th line of file.
- * If you want to insert a line, append newline '\n' to the end of the string.
- */
 func InsertStringToFile(path, str string, index int) error {
-	lines, err := File2lines(path)
-	if err != nil {
-		return err
-	}
-
-	fileContent := ""
-	for i, line := range lines {
-		if i == index {
-			fileContent += str
+	var lines []string
+	{
+		f, err := os.Open(path)
+		if err != nil {
+			log.Print(err)
 		}
-		fileContent += line
-		fileContent += "\n"
+
+		scanner := bufio.NewScanner(f)
+
+		for scanner.Scan() {
+			lines = append(lines, scanner.Text())
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Print(err)
+		}
+
+		defer f.Close()
 	}
 
-	return ioutil.WriteFile(path, []byte(fileContent), 0644)
+	lines[index] = str + "\n" + lines[index]
+
+	result := strings.Join(lines, "\n")
+
+	return ioutil.WriteFile(path, []byte(result), 0644)
 }
